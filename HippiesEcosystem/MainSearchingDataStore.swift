031d6abb865b03b8e62491b2b09385094670b223
Daniel <messyjones@gmail.com>
2017-06-20 20:44:38 -1000

    I created a superclass to hold all search functionality

--- HippiesEcosystem/MainSearchingDataStore.swift
+++ HippiesEcosystem/MainSearchingDataStore.swift
@@ -10,16 +10,29 @@ import Foundation
 import Parse
 
 protocol MainSearchingDelegate {
-    func passSearchResults(results: [ProductType])
+    func passSearchResults(results: [Any])
     func getMostCurrentSearchText() -> String?
 }
 
 class MainSearchingDataStore {
+    enum SearchType: String {
+        case productType = "searchProduct"
+        case color = "searchColor"
+    }
+    
     var delegate: MainSearchingDelegate?
     fileprivate var isSearching = false
     
     init(delegate: MainSearchingDelegate) {
         self.delegate = delegate
+    }
+    
+    func querySearchResults(text: String) {
+        fatalError("must override in subclasses")
+    }
+    
+    func transform(results: Any) -> [Any] {
+        return []
     }
 }
 
@@ -28,20 +41,22 @@ extension MainSearchingDataStore {
     func search(text: String) {
         if !isSearching {
             isSearching = true
-            let lowercasedText = text.lowercased()
-            PFCloud.callFunction(inBackground: "searchProduct", withParameters: ["searchText": lowercasedText], block: {
-                (results: Any?, error: Error?) -> Void in
-                if let productTypeParses = results as? [ProductTypeParse] {
-                    let productTypes = productTypeParses.map({ (p: ProductTypeParse) -> ProductType in
-                        return ProductType(p: p)
-                    })
-                    self.delegate?.passSearchResults(results: productTypes)
-                } else if let error = error {
-                    print(error)
-                }
-                self.compareToNewestSearch(queriedSearchText: text)
-            })
+            querySearchResults(text: text)
         }
+    }
+    
+    func search(text: String, type: SearchType) {
+        let lowercasedText = text.lowercased()
+        PFCloud.callFunction(inBackground: type.rawValue, withParameters: ["searchText": lowercasedText], block: {
+            (results: Any?, error: Error?) -> Void in
+            if let results = results {
+                let searchResults = self.transform(results: results)
+                self.delegate?.passSearchResults(results: searchResults)
+            } else if let error = error {
+                print(error)
+            }
+            self.compareToNewestSearch(queriedSearchText: text)
+        })
     }
     
     fileprivate func compareToNewestSearch(queriedSearchText: String) {
--- HippiesEcosystem/ProductSearchViewController.swift
+++ HippiesEcosystem/ProductSearchViewController.swift
@@ -7,27 +7,14 @@
 //
 
 import UIKit
-import EZSwiftExtensions
 
-class ProductSearchViewController: UIViewController {
+class ProductSearchViewController: MainSearchingViewController {
     enum ResulAction {
         case remove
         case input
         case multipleCut
     }
     
-    var theSearchBar: CustomSearchBar!
-    var theTableView: UITableView!
-    var theSpinnerView: UIView?
-    
-    var results: [ProductType] = [] {
-        didSet {
-            hasLoadedDatabaseOnce = true
-        }
-    }
-    
-    var hasLoadedDatabaseOnce: Bool = false
-    var dataStore: MainSearchingDataStore?
     var resultAction: ResulAction = .input
     
     init(resultAction: ResulAction) {
@@ -41,18 +28,6 @@ class ProductSearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewSetup()
-        dataStoreSetup()
-    }
-    
-    fileprivate func viewSetup() {
-        let searchingView = MainSearchingView(frame: self.view.bounds, navBarHeight: navigationBarHeight + ez.screenStatusBarHeight)
-        self.view = searchingView
-        theTableView = searchingView.theTableView
-        theTableView.delegate = self
-        theTableView.dataSource = self
-        theSearchBar = searchingView.theSearchBar
-        theSearchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,44 +35,20 @@ class ProductSearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func dataStoreSetup() {
-        dataStore = MainSearchingDataStore(delegate: self)
-    }
-}
-
-extension ProductSearchViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results.count
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        var numOfSections: Int = 0
-        if results.isEmpty && hasLoadedDatabaseOnce {
-            let noDataLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
-            noDataLabel.text = "No results found"
-            noDataLabel.textColor = UIColor.white
-            noDataLabel.textAlignment = .center
-            tableView.backgroundView  = noDataLabel
-        } else {
-            numOfSections = 1
-            tableView.backgroundView = nil
-        }
-        return numOfSections
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let productType = results[indexPath.row]
+    override func createCell(indexPath: IndexPath) -> MainSearchTableViewCell {
+        let productType = results[indexPath.row] as! ProductType
         let cell = MainSearchTableViewCell(title: productType.title)
-        cell.selectionStyle = .none
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60.0
+    override func dataStoreSetup() {
+        dataStore = ProductSearchDataStore(delegate: self)
     }
-    
+}
+
+extension ProductSearchViewController {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let productType = results[indexPath.row]
+        let productType = results[indexPath.row] as! ProductType
         segueToInventoryVC(product: productType)
     }
     
@@ -115,22 +66,5 @@ extension ProductSearchViewController: UITableViewDelegate, UITableViewDataSourc
         
         targetVC.title = product.title
         pushVC(targetVC)
-    }
-}
-
-extension ProductSearchViewController: UISearchBarDelegate {
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        dataStore?.search(text: searchText)
-    }
-}
-
-extension ProductSearchViewController: MainSearchingDelegate {
-    func passSearchResults(results: [ProductType]) {
-        self.results = results
-        theTableView.reloadData()
-    }
-    
-    func getMostCurrentSearchText() -> String? {
-        return theSearchBar.text
     }
 }
