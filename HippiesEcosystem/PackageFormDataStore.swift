9941ba13d10870220e8e837eaed1c030290687b0
Daniel <messyjones@gmail.com>
2017-06-25 23:46:19 -1000

    fixed package input form to create a potential pick list

--- HippiesEcosystem/PackageFormDataStore.swift
+++ HippiesEcosystem/PackageFormDataStore.swift
@@ -7,8 +7,20 @@
 //
 
 import Foundation
+import Parse
 
 class PackageFormDataStore: TrackingFormDataStore {
+    override func save(id: Double) {
+        PFCloud.callFunction(inBackground: "inputPackage", withParameters: ["uniqueItemID": id], block: {
+            (results: Any?, error: Error?) -> Void in
+            if let _ = results {
+                self.delegate?.successfullySaved()
+            } else if let error = error {
+                self.delegate?.recieved(error: error)
+            }
+        })
+    }
+    
     override func recieved(itemParse: ItemParse) {
         let package = Package()
         package.state = .waiting_for_identified_pick
